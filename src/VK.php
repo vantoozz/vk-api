46152533ae722c38d36cb57b454e7b75c4a13055
Ivan Nikitin <vantoozz@gmail.com>
2013-06-07 13:00:03 +0400

    + parseCookie method

--- src/VK.php
+++ src/VK.php
@@ -34,6 +34,45 @@ class VK{
 		return $response->response;
 	}
 	
+	private function parseCookie($this->app_id) {
+        $session = array();
+        $member = FALSE;
+        if(!isset($_COOKIE['vk_app_'.$this->app_id]))return false;
+        $valid_keys = array('expire', 'mid', 'secret', 'sid', 'sig');
+        $app_cookie = $_COOKIE['vk_app_'.$this->app_id];
+        if ($app_cookie) {
+            $session_data = explode ('&', $app_cookie, 10);
+            foreach ($session_data as $pair) {
+                list($key, $value) = explode('=', $pair, 2);
+                if (empty($key) || empty($value) || !in_array($key, $valid_keys)) {
+                    continue;
+                }
+                $session[$key] = $value;
+            }
+            foreach ($valid_keys as $key) {
+                if (!isset($session[$key])) return $member;
+            }
+            ksort($session);
+
+            $sign = '';
+            foreach ($session as $key => $value) {
+                if ($key != 'sig') {
+                    $sign .= ($key.'='.$value);
+                }
+            }
+            $sign .= $this->secret;
+            $sign = md5($sign);
+            if ($session['sig'] == $sign && $session['expire'] > time()) {
+                $member = array(
+                    'id' => intval($session['mid']),
+                    'secret' => $session['secret'],
+                    'sid' => $session['sid']
+                );
+            }
+        }
+        return $member;
+    }
+	
 	public function cookie(){
 		if(!isset($_COOKIE['vk_app_'.$this->app_id])){
 			throw new VKException('No VK cookie');
